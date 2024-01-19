9839b4e979056faac30cbf1d37aa3fb072837c14
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-01-19 15:15:09 -0300

    Add game timer and move counter

--- js/main.js
+++ js/main.js
@@ -11,9 +11,42 @@ let interval;
 let fisrtCard = false;
 let secondCard = false;
 
-class Card {
-    constructor(name, image) {
-        this.name = name;
-        this.image = image;
+const items = [
+    {name: "bee", img: "img/bee.png"},
+    {name:"crocodile", img: "img/crocodile.png"},
+    {name:"macaw", img: "img/macaw.png"},
+    {name:"gorilla", img: "img/gorilla.png"},
+    {name:"tiger", img: "img/tiger.png"},
+    {name:"monkey", img: "img/monkey.png"},
+    {name:"chameleon", img: "img/chameleon.png"},
+    {name:"piranha", img: "img/piranha.png"},
+    {name:"anaconda", img: "img/anaconda.png"},
+    {name:"sloth", img: "img/sloth.png"},
+    {name:"cockatoo", img: "img/cockatoo.png"},
+    {name:"toucan", img: "img/toucan.png"},
+]
+
+let seconds = 0,
+    minutes = 0;
+
+let movesCount = 0,
+    winCounter = 0;
+
+const timeGenerator = () => {
+    seconds++;
+
+    if(seconds / 60 === 1) {
+        seconds = 0;
+        minutes++;
     }
+}
+
+let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+let minutesValues = minutes < 10 ? `0${minutes}` : minutes;
+
+timeValue.innerHTML = `<span>Time:</span?> ${minutesValues}:${secondsValue}`;
+
+const movesCounter = () => {
+    // movesCount++;
+    moves.innerHTML = `<span>Moves:</span> ${++movesCount}`
 }
