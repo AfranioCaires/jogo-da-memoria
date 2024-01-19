677d9a9fc7761009364d39606ea9a62b5d68c753
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-01-19 20:54:33 -0300

    Add flipping cards layout

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const moves = document.getElementById('moves-counter');
-const timeValue = document.getElementById('timer');
-const startBtn = document.getElementById('start-game');
-const stopBtn = document.getElementById('stop-game');
-const gameContainer = document.querySelector('.game-container');
-const result = document.getElementById('result');
-const controls = document.querySelector('.controls-container');
+const moves = document.getElementById("moves-counter");
+const timeValue = document.getElementById("timer");
+const startBtn = document.getElementById("start-game");
+const stopBtn = document.getElementById("stop-game");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
 
 let cards;
 let interval;
@@ -12,34 +12,34 @@ let fisrtCard = false;
 let secondCard = false;
 
 const items = [
-    {name: "bee", img: "img/bee.png"},
-    {name:"crocodile", img: "img/crocodile.png"},
-    {name:"macaw", img: "img/macaw.png"},
-    {name:"gorilla", img: "img/gorilla.png"},
-    {name:"tiger", img: "img/tiger.png"},
-    {name:"monkey", img: "img/monkey.png"},
-    {name:"chameleon", img: "img/chameleon.png"},
-    {name:"piranha", img: "img/piranha.png"},
-    {name:"anaconda", img: "img/anaconda.png"},
-    {name:"sloth", img: "img/sloth.png"},
-    {name:"cockatoo", img: "img/cockatoo.png"},
-    {name:"toucan", img: "img/toucan.png"},
-]
+  { name: "bee", img: "img/bee.png" },
+  { name: "crocodile", img: "img/crocodile.png" },
+  { name: "macaw", img: "img/macaw.png" },
+  { name: "gorilla", img: "img/gorilla.png" },
+  { name: "tiger", img: "img/tiger.png" },
+  { name: "monkey", img: "img/monkey.png" },
+  { name: "chameleon", img: "img/chameleon.png" },
+  { name: "piranha", img: "img/piranha.png" },
+  { name: "anaconda", img: "img/anaconda.png" },
+  { name: "sloth", img: "img/sloth.png" },
+  { name: "cockatoo", img: "img/cockatoo.png" },
+  { name: "toucan", img: "img/toucan.png" },
+];
 
 let seconds = 0,
-    minutes = 0;
+  minutes = 0;
 
 let movesCount = 0,
-    winCount = 0;
+  winCount = 0;
 
 const timeGenerator = () => {
-    seconds++;
+  seconds++;
 
-    if(seconds / 60 === 1) {
-        seconds = 0;
-        minutes++;
-    }
-}
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
+  }
+};
 
 let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
 let minutesValues = minutes < 10 ? `0${minutes}` : minutes;
@@ -47,52 +47,46 @@ let minutesValues = minutes < 10 ? `0${minutes}` : minutes;
 timeValue.innerHTML = `<span>Time:</span> ${minutesValues}:${secondsValue}`;
 
 const movesCounter = () => {
-    // movesCount++;
-    moves.innerHTML = `<span>Moves:</span> ${++movesCount}`
-}
+  // movesCount++;
+  moves.innerHTML = `<span>Moves:</span> ${++movesCount}`;
+};
 
 const generateRadom = (size = 4) => {
-    let tempArr = [...items];
-    let cardValues = []
+  let tempArr = [...items];
+  let cardValues = [];
 
-    size = size * size / 2;
-    for (let i = 0; i < size; i++) {
-        let randomIndex = Math.floor(Math.random() * tempArr.length);
-        cardValues.push(tempArr[randomIndex]);
-        tempArr.splice(randomIndex, 1);
-    }
-    return cardValues;
-}
+  size = (size * size) / 2;
+  for (let i = 0; i < size; i++) {
+    let randomIndex = Math.floor(Math.random() * tempArr.length);
+    cardValues.push(tempArr[randomIndex]);
+    tempArr.splice(randomIndex, 1);
+  }
+  return cardValues;
+};
 
 const matrixGenerator = (cardValues, size = 4) => {
-    gameContainer.innerHTML = ``;
-    cardValues = [...cardValues, ...cardValues];
-    cardValues.sort(() => Math.random() - 0.5);
-    for (let i = 0; i < size * size; i++) {
-        gameContainer.innerHTML += `
+  gameContainer.innerHTML = ``;
+  cardValues = [...cardValues, ...cardValues];
+  cardValues.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < size * size; i++) {
+    gameContainer.innerHTML += `
         <div class="card-container" data-card-value="${cardValues[i].name}">
             <div class="card-before">?</div>
-            <div class="card=after">
+            <div class="card-after">
                 <img src="${cardValues[i].img}" alt="${cardValues[i].name}">
             </div>
         </div>
         `;
-    }
-    gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
-}
+  }
+  gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+};
 
 const initializer = () => {
-    result.innerText = '';
-    winCount = 0;
-    let cardValues = generateRadom();
-    console.log(cardValues);
-    matrixGenerator(cardValues);
-
-}
-
+  result.innerText = "";
+  winCount = 0;
+  let cardValues = generateRadom();
+  console.log(cardValues);
+  matrixGenerator(cardValues);
+};
 
 initializer();
-
-
-
-
